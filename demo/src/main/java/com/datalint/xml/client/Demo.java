813c33a4c6029b9fbbed6ebbcdd678b79568f68c
Hua Zhang <hzhang1978@gmail.com>
2020-12-26 13:19:06 +0100

    add demo project

---
+++ demo/src/main/java/com/datalint/xml/client/Demo.java
@@ -0,0 +1,12 @@
+package com.datalint.xml.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class Demo implements EntryPoint {
+	@Override
+	public void onModuleLoad() {
+		RootPanel.get().add(new Label("Hello, world!"));
+	}
+}
