4d97b194d68b72a738f581195ae00eb8a2436675
Hua Zhang <hzhang1978@gmail.com>
2020-12-28 09:57:08 +0100

    try different pom settings

--- demo/src/main/java/com/datalint/xml/client/Demo.java
+++ demo/src/main/java/com/datalint/xml/client/Demo.java
@@ -1,5 +1,6 @@
 package com.datalint.xml.client;
 
+import com.datalint.xml.shared.XmlParser;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -7,6 +8,6 @@ import com.google.gwt.user.client.ui.RootPanel;
 public class Demo implements EntryPoint {
 	@Override
 	public void onModuleLoad() {
-		RootPanel.get().add(new Label("Hello, world!"));
+		RootPanel.get().add(new Label("Hello, world!" + XmlParser.parse("<a/>")));
 	}
 }
