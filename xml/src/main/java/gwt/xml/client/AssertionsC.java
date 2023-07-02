35fcc8c16d50c4128309f40fd2699711845849c3
Hua Zhang <hzhang1978@gmail.com>
2023-07-02 18:30:34 +0200

    improve AssertionsC and update domino version

--- xml/src/main/java/gwt/xml/client/AssertionsC.java
+++ xml/src/main/java/gwt/xml/client/AssertionsC.java
@@ -4,48 +4,66 @@ import gwt.xml.shared.ICommon;
 import org.dominokit.domino.ui.style.Color;
 import org.dominokit.domino.ui.typography.Paragraph;
 import org.dominokit.domino.ui.utils.DominoElement;
+import org.jboss.elemento.IsElement;
 
 import java.util.Objects;
 import java.util.function.BiPredicate;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class AssertionsC implements ICommon {
-    public static <A> void assertEquals(A actual, Predicate<A> predicate, String message) {
+    private static Consumer<IsElement<?>> appendChildConsumer;
+
+    public static void setAppendChildConsumer(Consumer<IsElement<?>> appendChildConsumer) {
+        AssertionsC.appendChildConsumer = appendChildConsumer;
+    }
+
+    public static void appendChild(IsElement<?> child) {
+        if (appendChildConsumer == null)
+            DominoElement.body().appendChild(child);
+        else
+            appendChildConsumer.accept(child);
+    }
+
+    public static <A> boolean assertEquals(A actual, Predicate<A> predicate, String message) {
+        boolean isPositive = predicate.test(actual);
         Paragraph paragraph;
-        if (predicate.test(actual))
+        if (isPositive)
             paragraph = Paragraph.create(createMessage(actual, true)).setColor(Color.GREEN);
         else
             paragraph = Paragraph.create(message == null ? createMessage(actual, false) : message).
                     setColor(Color.RED);
 
-        DominoElement.body().appendChild(paragraph);
+        appendChild(paragraph);
+
+        return isPositive;
     }
 
-    public static <A> void assertEquals(A actual, Predicate<A> predicate) {
-        assertEquals(actual, predicate, (String) null);
+    public static <A> boolean assertEquals(A actual, Predicate<A> predicate) {
+        return assertEquals(actual, predicate, (String) null);
     }
 
-    public static <E, A> void assertEquals(E expected, A actual, BiPredicate<E, A> predicate, String message) {
-        appendMessage(expected, actual, predicate.test(expected, actual), false, message);
+    public static <E, A> boolean assertEquals(E expected, A actual, BiPredicate<E, A> predicate, String message) {
+        return appendMessage(expected, actual, predicate.test(expected, actual), false, message);
     }
 
-    public static <E, A> void assertEquals(E expected, A actual, BiPredicate<E, A> predicate) {
-        assertEquals(expected, actual, predicate, null);
+    public static <E, A> boolean assertEquals(E expected, A actual, BiPredicate<E, A> predicate) {
+        return assertEquals(expected, actual, predicate, null);
     }
 
-    public static void assertEquals(Object expected, Object actual) {
-        assertEquals(expected, actual, (String) null);
+    public static boolean assertEquals(Object expected, Object actual) {
+        return assertEquals(expected, actual, (String) null);
     }
 
-    public static void assertEquals(Object expected, Object actual, String message) {
-        assertEquals(expected, actual, Objects::equals, message);
+    public static boolean assertEquals(Object expected, Object actual, String message) {
+        return assertEquals(expected, actual, Objects::equals, message);
     }
 
-    public static <E, A> void assertNotEquals(E expected, A actual, BiPredicate<E, A> predicate, String message) {
-        appendMessage(expected, actual, !predicate.test(expected, actual), true, message);
+    public static <E, A> boolean assertNotEquals(E expected, A actual, BiPredicate<E, A> predicate, String message) {
+        return appendMessage(expected, actual, !predicate.test(expected, actual), true, message);
     }
 
-    private static <E, A> void appendMessage(E expected, A actual, boolean isPositive, boolean mask, String message) {
+    private static <E, A> boolean appendMessage(E expected, A actual, boolean isPositive, boolean mask, String message) {
         Paragraph paragraph;
         if (isPositive)
             paragraph = Paragraph.create(createMessage(expected, actual, !mask)).setColor(Color.GREEN);
@@ -53,27 +71,33 @@ public class AssertionsC implements ICommon {
             paragraph = Paragraph.create(message == null ? createMessage(expected, actual, mask) : message)
                     .setColor(Color.RED);
 
-        DominoElement.body().appendChild(paragraph);
+        appendChild(paragraph);
+
+        return isPositive;
     }
 
-    public static <E, A> void assertNotEquals(E expected, A actual, BiPredicate<E, A> predicate) {
-        assertNotEquals(expected, actual, predicate, null);
+    public static <E, A> boolean assertNotEquals(E expected, A actual, BiPredicate<E, A> predicate) {
+        return assertNotEquals(expected, actual, predicate, null);
     }
 
-    public static void assertNotEquals(Object expected, Object actual) {
-        assertNotEquals(expected, actual, (String) null);
+    public static boolean assertNotEquals(Object expected, Object actual) {
+        return assertNotEquals(expected, actual, (String) null);
     }
 
-    public static void assertNotEquals(Object expected, Object actual, String message) {
-        assertNotEquals(expected, actual, Objects::equals, message);
+    public static boolean assertNotEquals(Object expected, Object actual, String message) {
+        return assertNotEquals(expected, actual, Objects::equals, message);
     }
 
-    public static void assertNull(Object actual) {
-        assertEquals(null, actual);
+    public static boolean assertNull(Object actual) {
+        return assertEquals(null, actual);
     }
 
-    public static void assertTrue(Object actual) {
-        assertEquals(Boolean.TRUE, actual);
+    public static boolean assertFalse(Object actual) {
+        return assertEquals(Boolean.FALSE, actual);
+    }
+
+    public static boolean assertTrue(Object actual) {
+        return assertEquals(Boolean.TRUE, actual);
     }
 
     private static String createMessage(Object actual, boolean equals) {
